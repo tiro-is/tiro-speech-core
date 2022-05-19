aedf66b2ec507a17ebad494bb43c7af116c8eacc
Róbert Kjaran <robert@kjaran.com>
2022-05-19 15:19:43 +0000

    Add flag to tiro_speech_server to print all model options
    The option --help-kaldi-models will print out an exhaustive list of options that
    can appear in main.conf for each model referenced by the --kaldi-models option,

--- src/bin/server_main.cc
+++ src/bin/server_main.cc
@@ -16,17 +16,38 @@
 #include <iostream>
 #include <string>
 
+#include "src/kaldi-model.h"
 #include "src/options.h"
 #include "src/server.h"
+
+namespace tiro_speech {
+
+template <typename Config>
+void PrintAllOptions(const char* message = "") {
+  ParseOptions po_model{message};
+  po_model.DisableOption("config");
+  po_model.DisableOption("help");
+  po_model.DisableOption("print-args");
+  po_model.DisableOption("verbose");
+  Config{}.Register(&po_model);
+  po_model.PrintUsage();
+}
+
+}  // namespace tiro_speech
 
 int main(int argc, char* argv[]) {
   try {
     using namespace tiro_speech;
 
     std::string log_level{"INFO"};
+    bool print_help_for_kaldi_models = false;
     ParseOptions po{"usage: lr-run-server"};
     po.Register("log-level", &log_level,
                 "Log level (one of DEBUG, INFO, WARNING, ERROR)");
+    po.Register(
+        "help-kaldi-model", &print_help_for_kaldi_models,
+        "Print all available configuration options for the models "
+        "loaded by --kaldi-models, i.e. options that can appear in main.conf");
 
     SpeechServerOptions server_options;
     server_options.Register(&po);
@@ -41,6 +62,11 @@ int main(int argc, char* argv[]) {
     po.PrintConfig(std::cerr);
 
     tiro_speech::logging::SetLogLevel(log_level);
+
+    if (print_help_for_kaldi_models) {
+      PrintAllOptions<KaldiModelConfig>("Kaldi model config:");
+      return EXIT_SUCCESS;
+    }
 
     SpeechServerInfo server_info{server_options};
     SpeechServer server{server_info};
